9a7a49327e4eb2f3b53cc9de5a3d1aa5d36e727d
Agustín <tomasmunizvozza@gmail.com>
2025-10-06 19:23:08 -0300

    Entrega 2

--- ecommerce-celulares/src/App.jsx
+++ ecommerce-celulares/src/App.jsx
@@ -1,13 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import "./app.css";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import "./App.css";
 
 function App() {
   return (
-    <div className="app">
-      <NavBar />
-      <ItemListContainer greeting="Bienvenidos a CellTECH 📱" />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <NavBar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ItemListContainer greeting="¡Bienvenido a CelularShop! 📱" />
+            }
+          />
+          <Route
+            path="/category/:categoryId"
+            element={<ItemListContainer greeting="Productos por Categoría" />}
+          />
+          <Route path="/item/:itemId" element={<ItemDetailContainer />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
